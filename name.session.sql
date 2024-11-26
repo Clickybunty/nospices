b4275f6c9121da203a66a40c6232838af50949f3
sik84 <stefan.sikiric@gmail.com>
2024-11-26 10:55:41 +0100

    123

--- name.session.sql
+++ name.session.sql
@@ -1,1 +1,7 @@
-SELECT JSON_TYPE(ids) FROM zutaten;
+INSERT INTO rezeptnames (id, rezeptname, youtube_link, chefkoch_link)
+VALUES (
+    'id:varchar',
+    'rezeptname:json',
+    'youtube_link:text',
+    'chefkoch_link:text'
+  );SELECT JSON_TYPE(ids) FROM zutaten;
